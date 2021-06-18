e06489c8bad0d670f14bb41c0319f662df8987d7
jofelipe <jofelipe@student.42sp.org.br>
2021-06-18 21:32:55 +0000

    byebyebasecamp

--- c05/ex03/ft_recursive_power.c
+++ c05/ex03/ft_recursive_power.c
@@ -6,7 +6,7 @@
 /*   By: jofelipe <jofelipe@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/06/17 18:03:39 by jofelipe          #+#    #+#             */
-/*   Updated: 2021/06/18 15:23:56 by jofelipe         ###   ########.fr       */
+/*   Updated: 2021/06/18 21:31:18 by jofelipe         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,5 +20,5 @@ int	ft_recursive_power(int nb, int power)
 		return (0);
 	if (power < 1)
 		return (0);
-	return (nb *= ft_recursive_power(3, power - 1));
+	return (nb *= ft_recursive_power(nb, power - 1));
 }
