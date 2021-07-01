6f88578b5c4025f9bb8ede41bfaeca4bcb6c3f11
VLN37 <jfprodriguez10@gmail.com>
2021-07-01 09:29:58 -0300

    c11

---
+++ c11/ex04/ft_is_sort.c
@@ -0,0 +1,14 @@
+int ft_is_sort(int *tab, int length, int(*f)(int, int))
+{
+	int	i;
+
+	i = 0;
+	while (i < length - 1)
+	{
+		if (f(tab[i], tab[i + 1]) < 0)
+			i++;
+		else
+			return (0);
+	}
+	return (1);
+}
---
+++ c11/ex04/main.c
@@ -0,0 +1,6 @@
+int ft_is_sort(int *tab, int length, int(*f)(int, int));
+
+int	main(void)
+{
+	return (0);
+}
